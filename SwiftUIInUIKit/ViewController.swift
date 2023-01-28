a0cc1c323fed3c9bf2073910571788c4e7d863de
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2023-01-28 18:53:03 +0100

    Initial Commit

---
+++ SwiftUIInUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIInUIKit
+//
+//  Created by Michele Manniello on 28/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
